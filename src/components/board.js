bd5f2aa783dd0f0c6d4de04917a77c198db9473d
yurii.kostenko <yurii.kostenko@agileengine.com>
2022-04-18 14:56:04 +0300

    Splitted into functional components

--- src/components/board.js
+++ src/components/board.js
@@ -5,34 +5,34 @@ const Board = (props) => {
     let squares = props.squares;
     let onClick = props.onClick;
 
-  let renderSquare = (i) => {
+    let renderSquare = (i) => {
+      return (
+        <Square
+          value={squares[i]}
+          onClick={() => onClick(i)}
+        />
+      );
+    };
+
     return (
-      <Square
-        value={squares[i]}
-        onClick={() => onClick(i)}
-      />
+      <div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
     );
   };
-
-  return (
-    <div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-    </div>
-  );
-};
 
 export default Board;
